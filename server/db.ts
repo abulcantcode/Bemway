357c0f8b26bb979c86fadd2f898e2a1e3e6f2bdf
Abul Chowdhury <abulchwdhury@gmail.com>
2023-11-12 16:44:24 +0000

    Docker run init scripts

--- server/db.ts
+++ server/db.ts
@@ -1,8 +1,9 @@
 import { Pool } from "pg";
+
 export const pool = new Pool({
-  host: "db",
-  port: 5432,
-  user: "user123",
-  password: "password123",
-  database: "db123",
+  host: "db", // Docker Compose service name
+  port: 5432, // PostgreSQL default port
+  user: "user123", // PostgreSQL username
+  password: "password123", // PostgreSQL password
+  database: "db123", // PostgreSQL database name
 });
---
+++ server/init-scripts/01-setup.sql
@@ -0,0 +1,5 @@
+CREATE TABLE schools (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    address VARCHAR(100)
+);
